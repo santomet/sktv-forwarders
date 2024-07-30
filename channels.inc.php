27c8136076e395710d78ca9d0b862a429951ce9a
Marek Šanta <teslan223@gmail.com>
2024-07-30 22:53:37 +0200

    Remove dead channels from channels.inc.php

--- channels.inc.php
+++ channels.inc.php
@@ -67,52 +67,9 @@ $czechNote = array(
 
 add_country($channels, "Czech Republic", "cz", $chan_cz, implode("\n", $czechNote));
 
-/*
-// Hungary
-$chan_hu = array();
-add_channel($chan_hu, "M1 /broken/", "M1", "https://hirado.hu/elo/m1/");
-add_channel($chan_hu, "M2 /broken/", "M2", "https://mediaklikk.hu/m2-elo/");
-add_channel($chan_hu, "M4 Sport + /broken/", "M4Plus", "https://m4sport.hu/elo/mtv4plus/");
-add_channel($chan_hu, "M5 /broken/", "M5", "https://mediaklikk.hu/m5-elo/");
-add_channel($chan_hu, "Duna /broken/", "Duna", "https://mediaklikk.hu/duna-elo/");
-add_channel($chan_hu, "Duna World /broken/", "Duna_World", "https://mediaklikk.hu/duna-world-elo/");
-
-add_country($channels, "Hungary", "hu", $chan_hu);
-*/
 
 // Portugal
 $chan_pt = array();
 add_channel($chan_pt, "CNN Portugal", "CNN_Portugal", "https://cnnportugal.iol.pt/direto");
 
 add_country($channels, "Portugal", "pt", $chan_pt);
-
-// Trinidad and Tobago
-$chan_tt = array();
-add_channel($chan_tt, "CNC3", "CNC3", "https://www.cnc3.co.tt/.live-stream/");
-add_channel($chan_tt, "gayelle", "gayelle", "https://tegostream.com/tv?channel=864");
-add_channel($chan_tt, "TTT", "TTT", "https://tegostream.com/tv?channel=255");
-add_channel($chan_tt, "Synergy TV", "Synergy_TV", "https://tegostream.com/tv?channel=259");
-add_channel($chan_tt, "IBN", "IBN", "https://tegostream.com/tv?channel=846");
-add_channel($chan_tt, "Trinity TV", "Trinity_TV", "https://tegostream.com/tv?channel=265");
-
-add_country($channels, "Trinidad and Tobago", "tt", $chan_tt);
-
-$chan_ge = array();
-add_channel($chan_ge, "Rustavi 2", "Rustavi2", "https://rustavi2.ge/en/dvr1");
-
-add_country($channels, "Georgia", "ge", $chan_ge);
-
-$chan_cl = array();
-add_channel($chan_cl, "TVN", "TVN", "https://www.tvn.cl/en-vivo");
-add_channel($chan_cl, "Chilevisión [broken]", "Chilevision", "https://www.chilevision.cl/senal-online");
-add_channel($chan_cl, "Canal 13", "Canal13", "https://www.13.cl/en-vivo");
-/*
-add_channel($chan_cl, "13 Entretención", "13Entretencion", "https://www.13.cl/13e");
-add_channel($chan_cl, "13 Cultura", "13Cultura", "https://www.13.cl/13cultura");
-add_channel($chan_cl, "13 Prime", "13Prime", "https://www.13.cl/13p");
-add_channel($chan_cl, "13 Kids", "13Kids", "https://www.13.cl/13kids");
-add_channel($chan_cl, "13 Realities", "13Realities", "https://www.13.cl/13realities");
-add_channel($chan_cl, "13 Teleseries", "13Teleseries", "https://www.13.cl/13t");
-*/
-
-add_country($channels, "Chile", "cl", $chan_cl);
